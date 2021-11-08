34051e0c6ed34e496ab31cd5705406f9e30ea340
alexberes <aalexbberes@gmail.com>
2021-11-08 12:07:36 -0800

    removed getuserroute

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -220,12 +220,4 @@ router.get("/resources", (req, res) => {
     })
 })
 
-
-
-
-router.get("/getallusers",(req,res)=>{
-    User.findAll().then(data=>{
-        res.json(data)
-    })
-})
 module.exports = router;
